41bf94061bb8bcfd8b84be9e3c8f1bbad2e4076b
pedrodls <pedrojoaodev@gmail.com>
2022-01-26 10:26:07 +0100

    User controller done

---
+++ controllers/user.controller.js
@@ -0,0 +1,22 @@
+const {
+    User
+} = require('../models/models')
+
+exports.all = async (req, res) => {
+    await User.findAll().then(u => res.json(u));
+}
+
+exports.one = async (req, res) => {
+    await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(u => res.json(u));
+}
+
+exports.create = async (req, res) => {
+
+    await User.create(req.body).
+    then(u => res.json(u)).
+    catch(e => res.json(e));
+}
